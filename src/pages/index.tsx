06ac8eba6a379098d667f3b3905fd45e7cd01ff4
Michael O'Toole <mikey@homotechsual.dev>
2025-04-21 22:00:05 +0100

    Rename button.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/">
-            Docusaurus Tutorial - 5min ⏱️
+            Knowledge Base
           </Link>
         </div>
       </div>
